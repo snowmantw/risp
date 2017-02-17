706812c5aad9927836a02937e3e581da778ba4de
Weng, ChengHui | Greg | TRVDD <chenghui.weng@rakuten.com>
2017-02-17 17:25:56 +0900

    WIP - also, revised the use of String may cause 24 bytes waste

--- src/nodes/src/node.rs
+++ src/nodes/src/node.rs
@@ -1,5 +1,8 @@
 use std::string::String;
 
 pub struct Node {
+    // TODO: Use string is just for convenient. Replace it for saving the 24 byte as much as possible.
+    // Maybe just a position of Lineno to the main text,
+    // Since a size fixed "string" cannot be adjusted according to the input length.
     pub text: String
 }
